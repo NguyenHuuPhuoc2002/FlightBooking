91b3eeefac05404895536bb4d49ea101059af9b7
nguyenhuuphuoc <nguyenhuuphuocpdl@gmail.com>
2025-03-30 19:03:23 +0700

    update

---
+++ FlightBooking/Controllers/HomesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            return Ok();
+        }
+    }
+    
+}
--- FlightBooking/Program.cs
+++ FlightBooking/Program.cs
@@ -1,11 +1,64 @@
+﻿using Microsoft.OpenApi.Models;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+#endregion
 
+#region Logging
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//Cấu hình logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console() 
+    .WriteTo.File("Logs/log.txt",
+        rollingInterval: RollingInterval.Day, 
+        retainedFileCountLimit: 7,
+        encoding: System.Text.Encoding.UTF8)
+    .CreateLogger();
+#endregion
+
+
+builder.Host.UseSerilog();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Cấu hình để sử dụng Bearer Token trong Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+
+});
 
 var app = builder.Build();
 
@@ -16,7 +69,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
